06eb745afe113cddd25f10079c4ea6658cbfaf59
hooman182 <hoomanm87@gmail.com>
2024-11-14 19:08:25 +0330

    feat: add custom user model, create manager, create forms & admin model of custom user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from users.forms import CustomUserChangeForm, CustomUserCreationForm
 
-# Register your models here.
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff",
+         "is_active", "groups", "user_permissions")}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+         ),
+    )
