827b4ef67e650649c3b7247c460c80c867832573
hooman <hoomanm87@gmail.com>
2025-06-25 20:18:50 +0330

    feat: create categories & incomes apps, create category & income models

--- core/settings.py
+++ core/settings.py
@@ -6,101 +6,100 @@ from corsheaders.defaults import default_headers, default_methods
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'test')
+SECRET_KEY = os.getenv("SECRET_KEY", "test")
 
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(' ')
-AUTH_USER_MODEL = 'users.CustomUser'
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(" ")
+AUTH_USER_MODEL = "users.CustomUser"
 
-CORS_ALLOWED_ORIGINS = os.getenv(
-    'CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(' ')
-
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000'
-# ]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000").split(
+    " "
+)
 
 CORS_ALLOW_METHODS = list(default_methods) + []
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
-    'accept-encoding',
-    'dnt',
-    'origin',
+    "accept-encoding",
+    "dnt",
+    "origin",
     "Authorization",
     "X-Custom-Header",
 ]
 
 # Project App's
 APPS = [
-    'users.apps.UsersConfig',
-    'authentication.apps.AuthenticationConfig',
-    'expense.apps.ExpenseConfig',
-    'collection.apps.CollectionConfig',
+    "users.apps.UsersConfig",
+    "authentication.apps.AuthenticationConfig",
+    "expense.apps.ExpenseConfig",
+    "collection.apps.CollectionConfig",
+    "incomes.apps.IncomesConfig",
+    "categories.apps.CategoriesConfig",
 ]
 
 # 3rd Party Packages
 INSTALLED_PACKAGES = [
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'djoser',
-
-    'drf_spectacular',
-    'corsheaders'
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "djoser",
+    "drf_spectacular",
+    "corsheaders",
 ]
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     *APPS,
     *INSTALLED_PACKAGES,
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.{}'.format(os.getenv('DATABASE_ENGINE', 'sqlite3')),
-        'NAME': os.getenv('DATABASE_NAME', 'hooman'),
-        'USER': os.getenv('DATABASE_USERNAME', 'hooman'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'hooman'),
-        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DATABASE_PORT', 1433),
+    "default": {
+        "ENGINE": "django.db.backends.{}".format(
+            os.getenv("DATABASE_ENGINE", "sqlite3")
+        ),
+        "NAME": os.getenv("DATABASE_NAME", "hooman"),
+        "USER": os.getenv("DATABASE_USERNAME", "hooman"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "hooman"),
+        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DATABASE_PORT", 1433),
     }
 }
 
@@ -108,25 +107,25 @@ DATABASES = {
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
 # Internationalization
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -134,42 +133,38 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Django Rest Framework config's
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 # JWT Auth variable's
-JWT_SECRET = os.getenv('JWT_SECRET')
-JWT_AUDIENCE = os.getenv('JWT_AUDIENCE')
-JWT_ISSUER = os.getenv('JWT_ISSUER')
+JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_AUDIENCE = os.getenv("JWT_AUDIENCE")
+JWT_ISSUER = os.getenv("JWT_ISSUER")
 
 # JWT Auth config's
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
-
     "ALGORITHM": "HS256",
     "SIGNING_KEY": JWT_SECRET,
     "JWT_AUDIENCE": JWT_AUDIENCE,
     "JWT_ISSUER": JWT_ISSUER,
     "JSON_ENCODER": None,
-
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
     "USER_ID_FIELD": "id",
@@ -178,15 +173,15 @@ SIMPLE_JWT = {
 
 # Djoser package config's
 DJOSER = {
-    'SERIALIZERS': {
-        'user_create': 'authentication.serializers.UserRegistrationSerializer'
+    "SERIALIZERS": {
+        "user_create": "authentication.serializers.UserRegistrationSerializer"
     }
 }
 
 # API Swagger Doc
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Bestoon API',
-    'DESCRIPTION': 'documentation of bestoons api',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
+    "TITLE": "Bestoon API",
+    "DESCRIPTION": "documentation of bestoons api",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
--- incomes/models.py
+++ incomes/models.py
@@ -1,24 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-from collection.models import Collection
-from categories.models import Category
-
 User = get_user_model()
 
 
-class Expense(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="category")
     collection = models.ForeignKey(
-        Collection, on_delete=models.CASCADE, related_name="expense"
+        "collection.Collection", on_delete=models.CASCADE, related_name="income"
     )
     title = models.CharField(max_length=200)
     description = models.TextField()
     amount = models.DecimalField(max_digits=12, decimal_places=3)
     date = models.DateField()
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="expense"
-    )
 
     created = models.DateTimeField(auto_now_add=True)
 
