424dc684481041158f0f2321fb9a11d58cfad251
hooman <hoomanm87@gmail.com>
2025-08-04 23:20:58 +0330

    refactor in expense views & serializer

--- expense/serializers.py
+++ expense/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 
+from collection.models import Collection
 from expense.models import Expense
+
+# -----------------------------------------------
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Expense
         fields = "__all__"
+        read_only_fields = ("user",)
+
+    def validate_collection(self, value):
+        user = self.context["request"].user
+
+        if value.user != user:
+            raise serializers.ValidationError("collection selected is wrong")
+
+        return value
--- expense/views.py
+++ expense/views.py
@@ -1,41 +1,22 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.shortcuts import get_object_or_404
+
 from expense.models import Expense
 from expense.serializers import ExpenseSerializer
-#------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------
 
 
-class ExpenseViewSet(viewsets.ViewSet):
-
-    permission_classes = []
+class ExpenseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-    
-    def list(self, request):
-        serializer = ExpenseSerializer(self.queryset, many=True)
-        return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        expense = get_object_or_404(Expense, pk=pk)
-        serializer = ExpenseSerializer(expense)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
 
-    def create(self, request):
-        serializer = ExpenseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg': 'expense created'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        if not self.request.user.is_authenticated:
+            raise ValueError("User is not authenticated")
 
-    def update(self, request, pk):
-        serializer = ExpenseSerializer(data=request.data)
-        expense = Expense.objects.get(pk=pk)
-        return Response({'msg': 'record updated'}, status=status.HTTP_200_OK)
-
-    def destroy(self, request, pk=None):
-        expense = Expense.objects.get(pk=pk)
-        expense.delete()
-        return Response({'msg': 'record deleted'}, status=status.HTTP_200_OK)
+        serializer.save(user=self.request.user)
