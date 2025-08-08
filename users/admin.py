c5508cbe7a58411870dcb8a8f881f2b9d583a315
hooman <hoomanm87@gmail.com>
2025-08-09 00:59:06 +0330

    feat: create profile model in user app, implement post save signal & add to admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from users.forms import CustomUserChangeForm, CustomUserCreationForm
-from users.models import CustomUser
+from users.models import CustomUser, Profile
 
 
 @admin.register(CustomUser)
@@ -9,21 +9,43 @@ class CustomUserAdmin(admin.ModelAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active', 'date_joined')
-    list_filter = ('is_staff', 'is_active', 'date_joined')
+    list_display = ("email", "is_staff", "is_active", "date_joined", "last_login")
+    list_filter = ("is_staff", "is_active", "date_joined", "last_login")
+
+    readonly_fields = ("last_login",)
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff",
-         "is_active", "groups", "user_permissions")}),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
     )
-    
+
     add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": (
-                "email", "password1", "password2", "is_staff",
-                "is_active", "groups", "user_permissions"
-            )}
-         ),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "first_name",
+        "last_name",
+        "birth",
     )
--- users/models.py
+++ users/models.py
@@ -3,19 +3,34 @@ from django.db import models
 
 from users.managers import CustomUserManager
 
+#------------------------------------------------------
 
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-    
-    USERNAME_FIELD = 'email'
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-    
+
     objects = CustomUserManager()
-    
+
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-        
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
     def __str__(self) -> str:
         return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, related_name="profile"
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth = models.DateField(blank=True, null=True)
+
+    address = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
