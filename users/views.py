dd0728c6981f37863c5e95cdda0c16f6ca3f1725
hooman <hoomanm87@gmail.com>
2025-11-19 16:04:49 +0330

    feat: create profile detail view by generic views (get, put, patch)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import  permissions, generics
+from .serializers import ProfileSerializer
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return self.request.user.profile
+
+    def perform_update(self, serializer):
+        
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("User is not authenticated")
+        
+        serializer.save(user=self.request.user)
