ef759860e894d0b720e58101764ec5879c0825b1
hooman <hoomanm87@gmail.com>
2025-11-19 16:05:02 +0330

    feat: add profile url

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
 
-docpatterns = [
+swagger_api_doc_patterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
@@ -12,8 +12,9 @@ docpatterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
-    path('api/expense/', include('expense.urls')),
-    path('api/collection/', include('collection.urls')),
+    path('api/users/', include('users.urls')),
     path('api/categories/', include('categories.urls')),
+    path('api/collection/', include('collection.urls')),
+    path('api/expense/', include('expense.urls')),
     path('api/incomes/', include('incomes.urls')),
-] + docpatterns
+] + swagger_api_doc_patterns
