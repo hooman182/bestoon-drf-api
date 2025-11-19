72b740f5f5336990b06a624ed4650b9570d9706b
hooman <hoomanm87@gmail.com>
2025-11-19 16:04:18 +0330

    feat: add new fields to Profile model (bio, phone, created, updated)

--- users/models.py
+++ users/models.py
@@ -29,8 +29,17 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth = models.DateField(blank=True, null=True)
-
+    
+    bio = models.TextField(blank=True, null=True)
+    
     address = models.CharField(max_length=200, blank=True, null=True)
-
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+        
+    def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
